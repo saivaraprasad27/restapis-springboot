bfa31bfa59e74bb9fd42d2542987d89f381b10ae
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2026-01-07 17:25:05 +0530

    Jpa related code

--- src/main/java/com/learn/DecProjectModule/models/Product.java
+++ src/main/java/com/learn/DecProjectModule/models/Product.java
@@ -1,7 +1,16 @@
 package com.learn.DecProjectModule.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Product {
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+public class Product extends BaseModel{
 
     /**
      * Model
@@ -14,72 +23,23 @@ public class Product {
      * HTML, JSP, Thymeleaf, etc.
      */
 
-    private Long id;
     private String title;
     private String description;
     private Double price;
     private String imageUrl;
+
+    @ManyToOne
     private Category category;
 
-    public Product(){
-
-    }
-
-    public Product(Long id, String title, String description, Double price, String imageUrl, Category category){
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.category = category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
+    @Override
+    public String toString() {
+        return "Product{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", category=" + category +
+                '}';
     }
 }
 
