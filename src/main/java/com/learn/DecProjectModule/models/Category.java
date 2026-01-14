e1b4dd772f6c249c3657164a887ad8627e445587
saivaraprasad27 <kondaka.Saivaraprasad@unifyapps.com>
2026-01-14 22:16:10 +0530

    Fetch data types: Eager and lazy data

--- src/main/java/com/learn/DecProjectModule/models/Category.java
+++ src/main/java/com/learn/DecProjectModule/models/Category.java
@@ -1,10 +1,15 @@
 package com.learn.DecProjectModule.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +18,8 @@ import lombok.Setter;
 @Entity
 public class Category extends BaseModel{
     private String title;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+    private List<Product> products;
+
 }
--- src/test/java/com/learn/DecProjectModule/DecProjectModuleApplicationTests.java
+++ src/test/java/com/learn/DecProjectModule/DecProjectModuleApplicationTests.java
@@ -1,6 +1,8 @@
 package com.learn.DecProjectModule;
 
+import com.learn.DecProjectModule.models.Category;
 import com.learn.DecProjectModule.models.Product;
+import com.learn.DecProjectModule.repository.CategoryRepository;
 import com.learn.DecProjectModule.repository.ProductRepository;
 import com.learn.DecProjectModule.repository.Projections.ProductProjection;
 import org.junit.jupiter.api.AutoClose;
@@ -15,6 +17,9 @@ class DecProjectModuleApplicationTests {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
 	@Test
 	void contextLoads() {
@@ -41,5 +46,18 @@ class DecProjectModuleApplicationTests {
         List<ProductProjection> product = productRepository.getProductByCategoryIdUsingProjections(1L);
         System.out.println(product.get(0).getId());
         System.out.println(product.get(0).getTitle());
+    }
+
+    @Test
+    void fetchTypeTest(){
+        Category category = categoryRepository.findById(1L).get();
+        System.out.println(category.getId());
+        System.out.println("We are done here");
+
+        List<Product> products = category.getProducts();
+        System.out.println(products.size());
+
+        System.out.println("We go the product as well");
+
     }
 }
